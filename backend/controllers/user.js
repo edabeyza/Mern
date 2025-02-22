fbf39058cf6fba810e20794fe740186c09808e3d
Beyza <edabeyzaozeren@gmail.com>
2025-02-22 03:50:39 +0300

    register controller done

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,9 +1,35 @@
-const User = require('./models/user.js')
+const User = require('../models/user.js')
 const bcrypt = require('bcryptjs')
 const jwt = require('bcryptjs')
 
 const register = async(req, res) => {
+    const {name, email, password} = req.body
 
+    const user = await User.findOne({email})
+    if(user){
+        return res.status(500).json({message: 'User already exist !!'})
+    }
+
+    const passwordHash = await bcrypt.hash(password, 10)
+
+    if(password.length < 6){
+        return res.status(500).json({message: 'Password cannot be less than 6 characters !!'})
+
+    }
+
+    const newUser = await User.create({name, email, password: passwordHash})
+
+    const token = await jwt.sign({id: newUser._id}, "SECRETTOKEN", {expiresIn: "lh"})
+
+    const cookieOptions = {
+        httpOnly: true,
+        expires: new Date(Date.now() + 5 + 24*60*60*1000)
+    }
+
+    res.status(201).cookie("token", token, cookieOptions).json({
+        newUser,
+        token
+    })
 }
 
 const login = async(req, res) => {
