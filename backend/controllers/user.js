506900cd221c8aab2acd35d0a167a72df8ec13de
Beyza <edabeyzaozeren@gmail.com>
2025-02-22 03:27:12 +0300

    user model done

---
+++ backend/controllers/user.js
@@ -0,0 +1,25 @@
+const User = require('./models/user.js')
+const bcrypt = require('bcryptjs')
+const jwt = require('bcryptjs')
+
+const register = async(req, res) => {
+
+}
+
+const login = async(req, res) => {
+    
+}
+
+const logout = async(req, res) => {
+    
+}
+
+const forgotPassword = async(req, res) => {
+    
+}
+
+const resetPassword = async(req, res) => {
+    
+}
+
+module.exports = {register, login, logout, forgotPassword, resetPassword}
--- backend/models/user.js
+++ backend/models/user.js
@@ -2,8 +2,42 @@ const mongoose = require('mongoose')
 
 const userSchema = new mongoose.Schema({
     name: {
-        type: String
+        type: String,
+        required: true
+    },
+
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+
+    password: {
+        type: String,
+        required: true,
+        minLength: 6
+    },
+
+    avatar: {
+        public_id: {
+            type: String,
+            required: true
+        },
+
+        url: {
+            type: String,
+            required: true
+        }
+    }, 
+
+    role: {
+        type: String,
+        default: "user",
+        required: true
     }
+}, 
+{
+    timestamps:true
 })
 
 module.exports = mongoose.model('User', userSchema)
